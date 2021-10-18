116c1281e7a33a9f56acb3864b92040d3b8513e4
mobius-crypt <mobiusndou@gmail.com>
2021-10-18 08:11:05 +0200

    created main application

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,10 @@
 from src.config import config_instance
 from flask import Flask
 
-def create_app(config_class=config_instance):
-    pass
+
+def create_app(config=config_instance):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    with app.app_context():
+        return app
