55fd10541b41a6862a16fd2784a1a91886bba43d
mobius-crypt <mobiusndou@gmail.com>
2021-10-18 21:34:00 +0200

    updated timesheets.py

---
+++ src/models/timesheets/timesheets.py
@@ -0,0 +1,16 @@
+"""
+    Time Sheets module allows
+        for keeping track of time worked by each bouncer
+"""
+from datetime import date, datetime
+from google.cloud import ndb
+
+from src.models.basemodel import BaseModel
+
+
+class TimeSheetModel(BaseModel):
+    uid: str = ndb.StringProperty(indexed=True)
+    today: date = ndb.DateProperty()
+    time_on_duty: datetime = ndb.DateTimeProperty()
+    time_of_duty: datetime = ndb.DateTimeProperty()
+    time_worked_minutes: int = ndb.IntegerProperty()
