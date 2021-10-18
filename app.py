6239f51dbe9469bee6f9aca5d70e7df4b62e71f8
mobius-crypt <mobiusndou@gmail.com>
2021-10-18 08:11:54 +0200

    added app.run

--- app.py
+++ app.py
@@ -3,3 +3,9 @@
 """
 import os
 
+from src.main import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8001)))
