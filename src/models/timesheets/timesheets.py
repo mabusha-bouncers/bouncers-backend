463ca80e1c4793a7f748817ab8b0a18ebfd8f106
mobius-crypt <mobiusndou@gmail.com>
2021-10-18 21:42:32 +0200

    update timesheets  added rates

--- src/models/timesheets/timesheets.py
+++ src/models/timesheets/timesheets.py
@@ -12,9 +12,18 @@ class TimeSheetModel(BaseModel):
     """
         **TimeSheetModel**
             keeps track of hours worked per bouncer / security guard
+
+        `PARAMETERS`
+            uid: unique user identifier
+            today: calendar date for today or day worked
+            time_on_duty: date -> the date the bouncer started work
+            time_of_duty: date -> the date the bouncer went of duty
+            time_worked_hours: integer -> total time worked in hours
+            hourly_rate: int (money in rands) -> hourly rate for bouncer
     """
     uid: str = ndb.StringProperty(indexed=True)
     today: date = ndb.DateProperty()
     time_on_duty: datetime = ndb.DateTimeProperty()
     time_of_duty: datetime = ndb.DateTimeProperty()
-    time_worked_minutes: int = ndb.IntegerProperty()
+    time_worked_hours: int = ndb.IntegerProperty()
+    hourly_rate: int = ndb.IntegerProperty()
