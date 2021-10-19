faca2409424da5f176565b22ee0f2440da371f7e
mobius-crypt <mobiusndou@gmail.com>
2021-10-19 06:24:23 +0200

    updated Address Model

--- src/models/address/address.py
+++ src/models/address/address.py
@@ -14,9 +14,19 @@ class AddressModel(BaseModel):
             holds addresses for clients, admins, bouncers and places where events will take place
             or where work is available
     """
-    address_id: str = ndb.StringProperty()
+    address_id: str = ndb.StringProperty(indexed=True)
     street: str = ndb.StringProperty()
     city_town: str = ndb.StringProperty()
     province: str = ndb.StringProperty()
     country: str = ndb.StringProperty()
     postal_code: str = ndb.StringProperty()
+
+    @property
+    def address(self) -> str:
+        return f"{self.street}, {self.city_town}, {self.province}, {self.country}, {self.postal_code}"
+
+    def __str__(self) -> str:
+        return f"<Address: {self.address}"
+
+    def __bool__(self) -> bool:
+        return bool(self.address_id)
