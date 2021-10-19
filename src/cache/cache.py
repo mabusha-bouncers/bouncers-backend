6836c7bcfb16ac3a6670ae33bf771bfe9a17c88b
mobius-crypt <mobiusndou@gmail.com>
2021-10-19 11:31:53 +0200

    initialized cache management

--- src/cache/cache.py
+++ src/cache/cache.py
@@ -7,12 +7,27 @@ __twitter__ = "@blueitserver"
 __github_profile__ = "https://github.com/freelancing-solutions/"
 __licence__ = "MIT"
 
+from src.config import config_instance
 from src.singleton import Singleton
+from flask_caching import Cache
 
 
 class CacheManager(Singleton):
-    def __init__(self, app):
-        pass
+    """
+    **CacheManager**
+            application specific cache manager
+
+    """
+
+    def __init__(self):
+        self._cache: Cache = Cache(config=config_instance.cache_dict())
+
+    def init_app(self, app):
+        self._cache.init_app(app=app)
+
+    @property
+    def cache(self):
+        return self._cache
 
 
 app_cache: CacheManager = CacheManager()
