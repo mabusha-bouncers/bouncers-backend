37e88f961cc1787b65cd11d415f3b895204131bc
mobius-crypt <mobiusndou@gmail.com>
2021-10-19 08:31:21 +0200

    updated ClientModel to include client_types + Notes

--- src/models/users/clients/clients.py
+++ src/models/users/clients/clients.py
@@ -3,6 +3,8 @@
         extends userModel and add client specific data
 """
 from google.cloud import ndb
+
+from src.models.address import AddressModel
 from src.models.users import UserModel
 
 
@@ -10,7 +12,20 @@ class ClientModel(UserModel):
     """
         **Clients Model**
             adds client specific functionality to UserModel
+
+        `PARAMETERS`
+            client_type: str -> the type of client either its business client or personal client
+            business: str -> Business Name if business Client
+            description: str -> detailed description of client
+            notes: str -> Notes about the client
     """
-    client_address: ndb.Key = ndb.KeyProperty()
+    client_type: str = ndb.StringProperty()
+    business: str = ndb.StringProperty()
+    description: str = ndb.StringProperty()
+    notes: str = ndb.StringProperty()
 
+    def __str__(self) -> str:
+        return f"{super().__str__()} client_type: {self.client_type}, Notes: {self.notes}"
 
+    def __bool__(self) -> bool:
+        return super().__bool__()
