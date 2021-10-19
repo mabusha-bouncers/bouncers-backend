ab0b3566fea141d365627a0910df3c89685a8545
mobius-crypt <mobiusndou@gmail.com>
2021-10-19 13:36:38 +0200

    added Enum Types and value lookups

--- src/models/address/address.py
+++ src/models/address/address.py
@@ -10,8 +10,26 @@ __twitter__ = "@blueitserver"
 __github_profile__ = "https://github.com/freelancing-solutions/"
 __licence__ = "MIT"
 
+from enum import Enum
+from typing import List
+
 from google.cloud import ndb
 from src.models.basemodel import BaseModel
+
+
+class SAProvinceTypes(Enum):
+    limpopo: str = 'limpopo'
+    mpumalanga: str = 'mpumalanga'
+    north_west: str = 'north_west'
+    gauteng: str = 'gauteng'
+    northern_cape: str = 'northern_cape'
+    kwazulu_natal: str = 'kwazulu_natal'
+    free_state: str = 'free_state'
+    eastern_cape: str = 'eastern_cape'
+
+    @classmethod
+    def types(cls) -> List:
+        return list(SAProvinceTypes)
 
 
 class AddressModel(BaseModel):
@@ -20,12 +38,12 @@ class AddressModel(BaseModel):
             holds addresses for clients, admins, bouncers and places where events will take place
             or where work is available
     """
-    address_id: str = ndb.StringProperty(indexed=True)
-    street: str = ndb.StringProperty()
-    city_town: str = ndb.StringProperty()
-    province: str = ndb.StringProperty()
-    country: str = ndb.StringProperty()
-    postal_code: str = ndb.StringProperty()
+    address_id: str = ndb.StringProperty(indexed=True, required=True)
+    street: str = ndb.StringProperty(indexed=True, required=True)
+    city_town: str = ndb.StringProperty(indexed=True, required=True)
+    province = ndb.StringProperty(choices=SAProvinceTypes.types(), indexed=True, required=True)
+    country: str = ndb.StringProperty(default='south africa', required=True)
+    postal_code: str = ndb.StringProperty(indexed=True, required=True)
 
     @property
     def address(self) -> str:
@@ -36,3 +54,8 @@ class AddressModel(BaseModel):
 
     def __bool__(self) -> bool:
         return bool(self.address_id)
+
+
+if __name__ == '__main__':
+    for province in SAProvinceTypes:
+        print(province)
