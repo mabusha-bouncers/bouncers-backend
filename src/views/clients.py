d6893eda380513394c025fd61455b5666ea91ccb
mobius-crypt <mobiusndou@gmail.com>
2021-11-02 13:49:49 +0200

    added client update method

--- src/views/clients.py
+++ src/views/clients.py
@@ -2,6 +2,7 @@
 **Clients API View
 """
 from flask import jsonify
+from google.cloud import ndb
 
 from src.exceptions import DataServiceError, status_codes
 from src.views import ViewModel
@@ -25,7 +26,7 @@ class ClientView(ViewModel):
         :param uid:
         :return:
         """
-        client_instance: ClientModel = ClientModel.query(ClientModel.uid == uid).first()
+        client_instance: ClientModel = ClientModel.query(ClientModel.uid == uid).get()
         if not isinstance(client_instance, ClientModel) or not bool(client_instance):
             raise DataServiceError(description='A Client with that ID does not exist please create a new account')
 
@@ -33,3 +34,31 @@ class ClientView(ViewModel):
         return jsonify(dict(status=True,
                             payload=client_instance.to_dict(),
                             message=_message)), status_codes.status_ok_code
+
+    @staticmethod
+    def post(client_details: dict):
+        """
+
+        :param client_details:
+        :return:
+        """
+        uid: str = client_details.get('uid')
+        client_instance: ClientModel = ClientModel.query(ClientModel.uid == uid).get()
+        if isinstance(client_instance, ClientModel) and bool(client_instance):
+            raise DataServiceError(description='A Client with that ID already exist')
+        
+        client_instance: ClientModel = ClientModel(**client_details)
+
+        _message: str = 'successfully created client'
+        return jsonify(dict(status=True,
+                            payload=client_instance.to_dict(),
+                            message=_message)), status_codes.status_ok_code
+
+
+    @staticmethod
+    def put(client_details: dict):
+        """
+            update client details
+        :param client_details:
+        :return:
+        """
