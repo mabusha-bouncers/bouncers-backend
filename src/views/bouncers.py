96ac2bdfa9fced647a1ca6860a5deec820275cd7
mobius-crypt <mobiusndou@gmail.com>
2021-11-02 08:52:29 +0200

    updated methods to create and update bouncers

--- src/views/bouncers.py
+++ src/views/bouncers.py
@@ -2,7 +2,9 @@
 **bouncers view**
 """
 from flask import jsonify
+from google.cloud import ndb
 
+from src.exceptions import InputError, DataServiceError
 from src.views import ViewModel
 from src.models.users import BouncerModel
 
@@ -25,13 +27,29 @@ class BouncersView(ViewModel):
             return jsonify(dict(status=False, message='unable to find bouncer with that id'))
         return jsonify(status=True, payload=bouncer_instance.to_dict(), message='bouncer successfully retrieved'), 200
 
-    def post(self, bouncer_details: dict):
+    @staticmethod
+    def post(bouncer_details: dict):
         """
             will create a bouncer from bouncer_details
         :param bouncer_details:
         :return:
         """
-        pass
+        # NOTE: bouncer uid should be supplied by the front end as part the login process
+        # will use firebase for login
+        uid: str = bouncer_details.get('uid')
+        bouncer_instance: BouncerModel = BouncerModel.query(BouncerModel.uid == uid).get()
+        if isinstance(bouncer_instance, BouncerModel) and bool(bouncer_instance):
+            raise DataServiceError(description='You Already have an account')
+
+        bouncer_instance: BouncerModel = BouncerModel(**bouncer_details)
+        key: ndb.Key = bouncer_instance.put()
+        if not isinstance(key, ndb.Key):
+            _message: str = 'Unable to create new User due to a technical error please try again later'
+            raise DataServiceError(description=_message)
+
+        return jsonify(status=True, payload=bouncer_instance.to_dict(), message='successfully created new bouncer')
+
+
 
     def put(self, bouncer_details: dict):
         """
