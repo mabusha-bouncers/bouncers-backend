350344b02e4f090b51d72f6fbb2da78aeb29e546
mobius-crypt <mobiusndou@gmail.com>
2021-11-03 08:58:24 +0200

    refactored bouncers list view

--- src/views/bouncers.py
+++ src/views/bouncers.py
@@ -9,7 +9,7 @@ from google.cloud import ndb
 from src.cache import app_cache
 from src.exceptions import InputError, DataServiceError
 from src.utils.utils import return_ttl
-from src.views import ViewModel
+from src.views import ViewModel, ListView
 from src.models.users import BouncerModel
 
 
@@ -90,34 +90,6 @@ class BouncerView(ViewModel):
         bouncer_instance.key.delete()
 
         return jsonify(status=True, message='user successfully deleted')
-
-
-class ListView(ViewModel):
-    """allows access to a list of bouncers"""
-    default_page_size: int = 10
-    methods = ['GET', 'POST']
-    method_decorators = []
-
-    def __init__(self):
-        super(ListView, self).__init__()
-        self.page_size: int = self.default_page_size
-
-    @staticmethod
-    @app_cache.cache.memoize(timeout=return_ttl('short'))
-    def bouncers_generator() -> List[BouncerModel]:
-        """this method will obtain and memoize the list of bouncers from the database"""
-        return [bouncer.to_dict() for bouncer in BouncerModel.query()]
-
-    def post(self, page_size: int):
-        """
-
-        :param page_size:
-        :return:
-        """
-        self.page_size = page_size
-        if not isinstance(page_size, int) or page_size < self.default_page_size:
-            raise InputError(description=f'Invalid Page size must be an integer not less than {self.default_page_size}')
-        return jsonify(status=True, message=f'page size successfully set to {self.page_size}')
 
 
 class BouncerListView(ListView):
