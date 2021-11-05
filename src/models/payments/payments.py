2954da2dce3fa1666dec84266670f4efb70f7a34
mobius-crypt <mobiusndou@gmail.com>
2021-11-05 08:11:21 +0200

    added payment types

--- src/models/payments/payments.py
+++ src/models/payments/payments.py
@@ -6,12 +6,32 @@ from google.cloud import ndb
 from src.models.mixins import AmountMixin
 from src.models.basemodel import BaseModel
 
+class PaymentTypes(Enum):
+    """
+        Enum for payment types
+    """
+    CASH = 1
+    CREDIT = 2
+    DEBIT = 3
+    
 
 class PaymentsModel(BaseModel):
-    """a model to track client payments"""
+    """
+    **Class PaymentsModel**
+        a model to track client payments
+
+    `Properties`
+        - **payment_id**: payment id
+        - **uid**: user id
+        - **amount**: amount paid
+        - **date**: date of payment
+        - **payment_type**: payment type
+    """
 
     uid: str = ndb.StringProperty()
+    payment_id: str = ndb.StringProperty()
     amount: AmountMixin = ndb.StructuredProperty(AmountMixin)
+    payment_type: str = ndb.StringProperty(choices=PaymentTypes.choices())
     date_paid: date = ndb.DateProperty(auto_now_add=True)
     time_paid: time = ndb.TimeProperty(auto_now_add=True)
 
