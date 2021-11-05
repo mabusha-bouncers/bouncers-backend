0ef2a0830c39445a3e5271671fb94fa2ddaea96c
mobius-crypt <mobiusndou@gmail.com>
2021-11-05 08:05:16 +0200

    added Payment Model

---
+++ src/models/mixins/__init__.py
@@ -0,0 +1,1 @@
+from src.models.mixins.mixins import AmountMixin, UserMixin, FeedbackMixin
--- src/models/payments/payments.py
+++ src/models/payments/payments.py
@@ -1,10 +1,24 @@
-
+"""
+    Module serves to create a payment object
+    in order to track client payments
+"""
+from google.cloud import ndb
+from src.models.mixins import AmountMixin
+from src.models.basemodel import BaseModel
 
 
 class PaymentsModel(BaseModel):
     """a model to track client payments"""
+
+    uid: str = ndb.StringProperty()
+    amount: AmountMixin = ndb.StructuredProperty(AmountMixin)
+    date_paid: date = ndb.DateProperty(auto_now_add=True)
+    time_paid: time = ndb.TimeProperty(auto_now_add=True)
+
     
     def __init__(self) -> None:
         super(PaymentsModel, self).__init__
     
-    
+
+    def __str__(self) -> str:
+        return f""
