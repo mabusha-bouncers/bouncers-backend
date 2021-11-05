aafcc05631db9931751f8173762b25ee8381d488
mobius-crypt <mobiusndou@gmail.com>
2021-11-06 00:11:56 +0200

    edited events on Properties

--- src/models/property/__init__.py
+++ src/models/property/__init__.py
@@ -33,11 +33,36 @@ class Events:
            events, and then create a method which will continuously fetch and execute
            the events asynchronously.
     """
+    
+    def __init__(self, events_server_url: str):
+        """
+            **__init__**
+                initializes the events server url
 
-    def __init__(self):
-        pass
+            :param events_server_url: the url of the events server
+        """
+        self.events_server_url = events_server_url
 
+    def trigger_event(self, event_name: str, event_data: dict):
+        """
+            **trigger_event**
+                triggers an event on the events server
 
+            :param event_name: the name of the event to trigger
+            :param event_data: the data to be sent to the event
+        """
+        requests.post(self.events_server_url, json={"event_name": event_name, "event_data": event_data})
+
+    def fetch_events(self):
+        """
+            **fetch_events**
+                fetches events from the events server
+
+            :return: the events fetched from the events server
+        """
+        return requests.get(self.events_server_url).json()
+
+        
 class Util:
     """
         **Class Util**
