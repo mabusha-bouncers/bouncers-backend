1f0b3a14f57237cd6dee5e937494bfc42460aeef
mobius-crypt <mobiusndou@gmail.com>
2021-11-07 12:19:44 +0200

    added Cron Jobs for Payments and PaySlips

---
+++ src/cron/timesheets.py
@@ -0,0 +1,64 @@
+"""
+    **Module TimeSheets Cron Jobs**
+        this module will run 
+
+"""
+
+class CalculateMonthlyPayroll:
+    """
+        **CalculateMonthlyPayroll**
+            this class will calculate the monthly payroll
+    """
+    def __init__(self):
+        """
+            **__init__**
+                this method will initialize the class
+        """
+        pass
+
+    def run(self):
+        """
+            **run**
+                this method will run the cron job
+        """
+        pass
+
+class CalculateWeeklyPayroll:
+    """
+        **CalculateWeeklyPayroll**
+            this class will calculate the weekly payroll    
+    """
+    def __init__(self):
+        """
+            **__init__**
+                this method will initialize the class
+        """
+        pass
+
+    def run(self):
+        """
+            **run**
+                this method will run the cron job
+        """
+        pass
+
+
+class CreatePaySlip:
+    """
+        **CreatePaySlip**
+            this class will create the payslip
+    """
+    def __init__(self):
+        """
+            **__init__**
+                this method will initialize the class
+        """
+        pass
+
+    def run(self):
+        """
+            **run**
+                this method will run the cron job
+        """
+        pass
+
